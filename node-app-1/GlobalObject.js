481794218e0439267cda1213a077e3a479253bcb
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-09 10:18:52 +0500

    global objects

---
+++ node-app-1/GlobalObject.js
@@ -0,0 +1,17 @@
+
+//Global
+
+console.log()
+setTimeout()
+clearTimeout()
+setInterval()
+clearInterval()
+
+//We can also call these object with global
+
+global.setTimeout()
+
+//Variable with global
+
+var message = "test"
+global.message
