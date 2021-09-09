02308d3ba635afdbdced7cd9aabef9b148383724
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-09-09 11:28:49 +0500

    creating module

---
+++ node-app-1/ModuleLogger.js
@@ -0,0 +1,9 @@
+var url = "http://mylogger.io/log";
+
+function log(message){
+    //send an http request
+    console.log(message)
+}
+
+module.exports.log = log;
+//or module.export.log = name-of-your-choice-to-be-used-in-other-file;
